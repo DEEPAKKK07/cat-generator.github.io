f4c11a02756b10a351927ab4da8d5407a1a5262d
DEEPAKKK07 <70376083+DEEPAKKK07@users.noreply.github.com>
2021-06-05 21:09:11 +0530

    cat

---
+++ cat.js
@@ -0,0 +1,6 @@
+function cat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-generator');
+    image.src = 'http://moar.edgecats.net/cats/1297064511_11.gif';
+    div.appendChild(image);
+}
